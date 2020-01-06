9742dbf1c93eb87b975469b03a5f704282ebc062
Will Carter <will.carter@fergusllc.com>
2020-01-06 16:24:59 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,10 +3,6 @@ class Person
     puts "Hello World!"
   end
   def walk
-<<<<<<< HEAD
     puts "The Person is walking"
-=======
-    puts "The Person is walking!"
->>>>>>> 9122a250221184a6c1ac23dda779d90d45fe9d2f
   end
 end
